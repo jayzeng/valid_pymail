7c5e1d717444a3b134160f7cac774217dfb716f3
Michael <michael@knockrentals.com>
2014-10-05 18:01:40 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import os
 from setuptools import setup
-
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "python-mailgun-validator",
@@ -17,11 +9,11 @@ setup(
     license = "BSD",
     keywords = "mailgun email validator API wrapper",
     url = "https://github.com/mjm159/python-mailgun-validator",
+    download_url = "https://github.com/mjm159/python-mailgun-validator/tarball/0.1",
     packages=['python-mailgun-validator'],
-    long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved ::  BSD License",
-    ],
+        "License :: OSI Approved :: BSD License"
+    ]
 )
