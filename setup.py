3775d5180c4085fc6ba092effa474eec7da7fb7b
Michael <michael@knockrentals.com>
2014-10-05 18:26:51 -0700

    editing to make PyPI compatible

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name = "python-mailgun-validator",
-    version = "0.1",
+    name = "valid_pymail",
+    version = "0.1.1",
     author = "Michael Malocha",
     author_email = "mmalocha13@gmail.com",
     description = ("A wrapper for the Mailgun email validator API"),
     license = "BSD",
     keywords = "mailgun email validator API wrapper",
     url = "https://github.com/mjm159/python-mailgun-validator",
-    download_url = "https://github.com/mjm159/python-mailgun-validator/tarball/0.1",
-    packages=['python-mailgun-validator'],
+    download_url = "https://github.com/mjm159/python-mailgun-validator/tarball/0.1.1",
+    packages=['valid_pymail'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
